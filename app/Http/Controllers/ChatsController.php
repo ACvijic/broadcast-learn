ce158e113630c2249ef52e5d1304d04746329544
Aleksa Cvijic <aleksa.cvijic@cubes.rs>
2018-02-11 11:20:21 +0100

    Configuring the ChatsController

---
+++ app/Http/Controllers/ChatsController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ *  @category   Controller
+ *  @package    Broadcast-learn
+ *  @author     Aleksa Cvijić <aleksa.cvijic@cubes.rs>
+ *  @copyright  2015-2018 Cubes d.o.o.
+ *  @version    1.0.0 One of the first classes implemented.
+ *  @link       http://broadcast.learn/
+ *  @since      Class available since Release 1.0.0
+ */
+class ChatsController extends Controller
+{
+    public function __construct()
+    {
+      $this->middleware('auth');
+    }
+
+    /**
+     *  Shows chats
+     *
+     *  @return \Illuminate\Http\Response
+     *  @access public
+     */
+    public function index()
+    {
+      return view('chat');
+    }
+
+    /**
+     *  Fetches all messages
+     *
+     *  @return Message
+     *  @access public
+     */
+    public function fetchMessages()
+    {
+      return Message::with('user')->get();
+    }
+
+    /**
+     *  Persists message to database
+     *
+     *  @param  \Illuminate\Http\Request $request
+     *  @return \Illuminate\Http\Response
+     *  @access public
+     */
+    public function sendMessage(Request $request)
+    {
+      $user = Auth::user();
+
+      $message = $user->messages()->create([
+        'message' => $request->input('message')
+      ]);
+
+      return ['status' => 'Message Sent!'];
+    }
+}
